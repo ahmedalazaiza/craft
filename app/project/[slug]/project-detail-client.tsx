5f5b415cc396adc64e16ef9c9fa62fd3489c967c
Ahmed Al-Azaiza <apple@MacBook-Pro-3.local>
2026-08-27 16:03:45 +0300

    style(project-detail): eliminate gap between stacked gallery images (0px seamless stack)

--- app/project/[slug]/project-detail-client.tsx
+++ app/project/[slug]/project-detail-client.tsx
@@ -218,10 +218,10 @@ export function ProjectDetailClient({ initialProject }: ProjectDetailClientProps
             </aside>
 
             {/* ------------------------------------------------------------- */}
-            {/* RIGHT/CENTER: Continuous Image List (1px Gap, Radius 0)        */}
+            {/* RIGHT/CENTER: Continuous Image List (0px Gap, Seamless Stack) */}
             {/* ------------------------------------------------------------- */}
             <main className="flex-1 min-w-0">
-              <div className="flex flex-col gap-[1px] w-full rounded-none overflow-hidden bg-[var(--border-neutral)] border-0 shadow-sm">
+              <div className="flex flex-col gap-0 w-full rounded-none overflow-hidden border-0 shadow-sm">
                 {allImages.map((img, idx) => (
                   <div
                     key={idx}
